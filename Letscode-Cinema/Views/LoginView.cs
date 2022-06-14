6eadf8643764e2c8ba59e5b715b71a6854b3c9d2
Joao Pedro Correa <joaopedrocorrea21@gmail.com>
2022-06-14 18:25:42 -0300

    initial commit

---
+++ Letscode-Cinema/Views/LoginView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letscode_Cinema.Views
+{
+    internal class LoginView
+    {
+        public void Show()
+        {
+            Console.WriteLine("Starting");
+        }
+    }
+}
