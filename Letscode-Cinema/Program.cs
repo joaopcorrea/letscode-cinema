b61b65cfa42044f52e5fcaf77c7359ccc7af46b9
Joao Pedro Correa <joaopedrocorrea21@gmail.com>
2022-06-20 15:41:49 -0300

    adding namespace to class program

--- Letscode-Cinema/Program.cs
+++ Letscode-Cinema/Program.cs
@@ -2,68 +2,51 @@
 using Letscode_Cinema.Services;
 using Letscode_Cinema.Views;
 
-Database db = new Database();
-db.CreateFiles();
-
-
-User user = new User();
-user.BirthDate = DateTime.Now.AddYears(-15);
-
-//Login login = new Login();
-//login.Show();
-
-//User user = new User();
-//user.BirthDate = DateTime.Now.AddYears(-15);
-
-//bool chosen = false;
-//while (!chosen)
-//{
-//    MovieList movieList = new MovieList();
-//    Movie movie = movieList.ChooseMovie();
-
-//    SessionList sessionList = new SessionList();
-//    try
-//    {
-//        Session session = sessionList.ChooseSession(movie, user);
-//        if (session != null)
-//        {
-//            Console.WriteLine("Sessao " + session.Id);
-//            chosen = true;
-//        }
-//    }
-//    catch (Exception ex)
-//    {
-//        Console.WriteLine(ex.Message);
-//    }
-//}
-
-bool chosen = false;
-while (!chosen)
+namespace Letscode_Cinema
 {
-    MovieList movieList = new MovieList();
-    Movie movie = movieList.ChooseMovie();
-
-    SessionList sessionList = new SessionList();
-    try
+    public class Program
     {
-        Session session = sessionList.ChooseSession(movie, user);
-        if (session != null)
+        public static void Main()
         {
-            Console.WriteLine("Sessao " + session.Id);
-            chosen = true;
+            Database db = new Database();
+            db.CreateFiles();
+
+            Login login = new Login();
+            login.Show();
+
+            User user = new User();
+            user.BirthDate = DateTime.Now.AddYears(-15);
+
+            bool chosen = false;
+            while (!chosen)
+            {
+                MovieList movieList = new MovieList();
+                Movie movie = movieList.ChooseMovie();
+
+                SessionList sessionList = new SessionList();
+                try
+                {
+                    Session session = sessionList.ChooseSession(movie, user);
+                    if (session != null)
+                    {
+                        Console.WriteLine("Sessao " + session.Id);
+                        chosen = true;
+                    }
+
+                    SeatList seatList = new SeatList();
+                    seatList.ChooseSeats(1);
+
+                    FoodList foodList = new FoodList();
+                    Dictionary<int, int> chosenFoods = foodList.ShowFoods();
+
+                    TicketList ticketList = new TicketList(chosenFoods);
+                    ticketList.ShowTicket();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
-
-        SeatList seatList = new SeatList();
-        seatList.ChooseSeats(1);
-
-        FoodList foodList = new FoodList();
-        Dictionary<int, int> chosenFoods = foodList.ShowFoods();
-      
-        TicketList ticketList = new TicketList(chosenFoods);
-        ticketList.ShowTicket();
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
     }
 }
