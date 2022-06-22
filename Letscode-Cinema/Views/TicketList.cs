fe3b21117a2bd3e107d4c7c5c457cd465fadec62
Lais Fernandes Daniel <laisfd@hotmail.com>
2022-06-21 22:47:02 -0300

    Update of TicketList Class

--- Letscode-Cinema/Views/TicketList.cs
+++ Letscode-Cinema/Views/TicketList.cs
@@ -13,14 +13,8 @@ namespace Letscode_Cinema.Views
         Cart cart;
 
         Ticket ticket = new Ticket();
-        //parametro como carrinho, somente colocar um for de quantidade, descrição
-        //lista antes com os tickets que possuem, imprimir o ticket
-        //Chamar método da database de criação de ticket, vincular com a classe ticket
-        //Receber ojeto da classe carrinho, pegar infos, novo objeto de ticket mandar pra database
-        //Carrinho.items.Quantidade = gerar ticket que via para o database
-==== BASE ====
-        public TicketList(Session pSession, List<int[]> pChosenSeats, Dictionary<int, int> pChosenFoods)
-==== BASE ====
+
+        public TicketList(Cart cart)
         {
             this.cart = cart;
         }
@@ -33,7 +27,6 @@ namespace Letscode_Cinema.Views
             do
             {
 
-                Console.Clear();
                 foreach (var t in tickets)
                 {
                     Console.WriteLine($"Ticket N {t.Id}");
@@ -69,7 +62,37 @@ namespace Letscode_Cinema.Views
                 Console.WriteLine($"{item.Id}\t{item.Quantity}x\t{item.Description}\t{item.TotalPrice}");
             }
 
-            Console.ReadLine();
+            //Console.WriteLine("Comida: ");
+            //double totalFoodValue = 0;
+            //foreach (var food in ticket.Foods)
+            //{
+            //    if (food.Key == 1)
+            //    {
+            //        Console.WriteLine($"{food.Key} Pipoca Salgada --- "
+            //            + $"Quantidade: {food.Value} --- Preço: {food.Value * 12}");
+            //        totalFoodValue += food.Value * 12;
+            //    }
+            //    else if (food.Key == 2)
+            //    {
+            //        Console.WriteLine($"{food.Key} Pipoca Doce ------ "
+            //            + $"Quantidade: {food.Value} --- Preço: {food.Value * 14}");
+            //        totalFoodValue += food.Value * 14;
+            //    }
+            //    else if (food.Key == 3)
+            //    {
+            //        Console.WriteLine($"{food.Key} Coca-Cola -------- "
+            //           + $"Quantidade: {food.Value} --- Preço: {food.Value * 5}");
+            //        totalFoodValue += food.Value * 5;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{food.Key} Coca-Cola Diet --- "
+            //           + $"Quantidade: {food.Value} --- Preço: {food.Value * 4.5}");
+            //        totalFoodValue += food.Value * 4.5;
+            //    }
+
+            //    Console.ReadLine();
+            //}
         }
 
         public void CreateTicket()
@@ -99,8 +122,6 @@ namespace Letscode_Cinema.Views
             }
         }
 
-
-
         //public TicketList(Session pSession, List<int[]> pChosenSeats, Dictionary<int, int> pChosenFoods)
         //{
         //    //ticket.UserId = pUser;
@@ -108,50 +129,6 @@ namespace Letscode_Cinema.Views
         //    ticket.Seats = pChosenSeats;
         //    ticket.FoodIds = pChosenFoods;
         //}
-
-        //public void ShowTicket()
-        //{
-        //    Console.WriteLine("-------------------------------------------------------");
-        //    Console.WriteLine("|------------------- T I C K E T ---------------------|");
-        //    Console.WriteLine("| Usuário: " + ticket.UserId);
-        //    Console.WriteLine("| Cinema: " + ticket.SessionId.Room.Cinema);
-        //    Console.WriteLine("| Data: " + ticket.SessionId.Date);
-        //    Console.Write("| Assento: " );
-        //    foreach (int[] seat in ticket.Seats)
-        //    {
-        //        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        //        Console.Write($"{letters[seat[0]]}{seat[1]}, ");
-        //    }
-        //    Console.WriteLine();
-        //    Console.WriteLine("| Comida: ");
-        //    double totalFoodValue = 0;
-        //    foreach (var food in ticket.FoodIds)
-        //    {
-        //        if (food.Key == 1)
-        //        {
-        //            Console.WriteLine($"| {food.Key} Pipoca Salgada --- "
-        //                + $"Quantidade: {food.Value} --- Preço: {food.Value * 12}");
-        //            totalFoodValue += food.Value * 12;
-        //        }
-        //        else if (food.Key == 2)
-        //        {
-        //            Console.WriteLine($"| {food.Key} Pipoca Doce ------ "
-        //                + $"Quantidade: {food.Value} --- Preço: {food.Value * 14}");
-        //            totalFoodValue += food.Value * 14;
-        //        }
-        //        else if (food.Key == 3)
-        //        {
-        //            Console.WriteLine($"| {food.Key} Coca-Cola -------- "
-        //               + $"Quantidade: {food.Value} --- Preço: {food.Value * 5}");
-        //            totalFoodValue += food.Value * 5;
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine($"| {food.Key} Coca-Cola Diet --- "
-        //               + $"Quantidade: {food.Value} --- Preço: {food.Value * 4.5}");
-        //            totalFoodValue += food.Value * 4.5;
-        //        }
-        //    }
         //    Console.WriteLine("|\n| Valor total da compra: " 
         //        + (totalFoodValue + ticket.Price).ToString("C") + "\n");
         //}
